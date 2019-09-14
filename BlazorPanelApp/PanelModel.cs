3078032bbab911dc91daa052829beb4af1cb3d9c
kasancode <kasancode@gmail.com>
2019-09-14 18:25:45 +0900

    1st

---
+++ BlazorPanelApp/PanelModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPanelApp
+{
+    public class PanelModel
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool On { get; set; }
+    }
+}
